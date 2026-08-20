12b5f4d8e6e177a133538cae611805d44c5144d7
Liototo <lionel.lansiart@gmail.com>
2026-08-20 13:12:40 +0200

    Add Cognito MVP auto-confirm trigger

---
+++ infra/cognito-pre-signup-auto-confirm.mjs
@@ -0,0 +1,7 @@
+export const handler = async (event) => {
+  event.response.autoConfirmUser = true;
+  if (event.request.userAttributes?.email) {
+    event.response.autoVerifyEmail = true;
+  }
+  return event;
+};
